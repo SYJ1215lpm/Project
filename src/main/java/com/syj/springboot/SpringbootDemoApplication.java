745138004da42e4064a1c69633e61e1db6f2a8b6
sunyj <sunyj@123.com>
2019-10-24 22:49:55 +0800

    Test Add

--- src/main/java/com/syj/springboot/SpringbootDemoApplication.java
+++ src/main/java/com/syj/springboot/SpringbootDemoApplication.java
@@ -12,5 +12,7 @@ public class SpringbootDemoApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(SpringbootDemoApplication.class, args);
+        System.out.println("1245");
     }
+
 }
