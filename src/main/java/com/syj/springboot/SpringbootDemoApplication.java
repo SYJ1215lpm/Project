d45fca490c968e3fb9c2703a80b2bb30c965e678
syj <syj@qq.com>
2019-10-25 09:36:28 +0800

    commit2

--- src/main/java/com/syj/springboot/SpringbootDemoApplication.java
+++ src/main/java/com/syj/springboot/SpringbootDemoApplication.java
@@ -13,7 +13,7 @@ public class SpringbootDemoApplication {
     public static void main(String[] args) {
         SpringApplication.run(SpringbootDemoApplication.class, args);
         System.out.println("git test");
-        System.out.println("1245");
+        System.out.println("124356");
     }
 
 }
